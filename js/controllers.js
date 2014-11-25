d0aa4bf52c92ea1c7921d04782213bfef3c42a02
Tim Jaroch <tim.jaroch@gmail.com>
2014-11-25 17:32:54 -0500

    Working to make portfolio page dynamic.

---
+++ js/controllers.js
@@ -0,0 +1,9 @@
+﻿var application = angular.module('application', []);
+
+application.controller('portfolioCtrl', function ($scope, $http) {
+    $scope.index = 0;
+    $http.get('blog/posts.json').success(function(data) {
+        $scope.Records = data;
+        $scope.Selected = $scope.Records["array"][$scope.index];
+    });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
 /* google maps */
 function initialize() {
-	var var_location = new google.maps.LatLng(42.9633333,-85.6680556);
+	var varLocation = new google.maps.LatLng(42.9633333,-85.6680556);
 	
-	roadAtlasStyles = [ { "featureType": "administrative", "stylers": [ { "visibility": "off" } ] },{ "featureType": "landscape", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road", "stylers": [ { "visibility": "simplified" }, { "color": "#102E14" } ] },{ "elementType": "labels.icon", "stylers": [ { "visibility": "off" } ] },{ "elementType": "labels.text", "stylers": [ { "visibility": "off" } ] },{ } ]
+	var roadAtlasStyles = [ { "featureType": "administrative", "stylers": [ { "visibility": "off" } ] },{ "featureType": "landscape", "stylers": [ { "visibility": "off" } ] },{ "featureType": "road", "stylers": [ { "visibility": "simplified" }, { "color": "#102E14" } ] },{ "elementType": "labels.icon", "stylers": [ { "visibility": "off" } ] },{ "elementType": "labels.text", "stylers": [ { "visibility": "off" } ] },{ } ];
 	
-	var var_mapoptions = {
+	var varMapoptions = {
 	  scrollwheel: false,
 	  draggable: false,
-	  center: var_location,
+	  center: varLocation,
 	  disableDefaultUI: true,
 	  zoom: 12
 	};
 
-	var var_map = new google.maps.Map(document.getElementById("map-container"),
-		var_mapoptions);
+	var varMap = new google.maps.Map(document.getElementById("map-container"), varMapoptions);
 	
-	var styledMapOptions = {
-                
-            };
+	var styledMapOptions = {};
 
 	var usRoadMapType = new google.maps.StyledMapType(
 		roadAtlasStyles, styledMapOptions);
 
-	var_map.mapTypes.set('usroadatlas', usRoadMapType);
-	var_map.setMapTypeId('usroadatlas');
+	varMap.mapTypes.set('usroadatlas', usRoadMapType);
+	varMap.setMapTypeId('usroadatlas');
 	
 	var mapText = document.createElement('button');
 	mapText.className = "btn btn-info btn-lg";
@@ -31,7 +28,7 @@ function initialize() {
 	var mapTextA = document.createElement('a');
 	mapTextA.setAttribute('href', "#about");
 	mapTextA.appendChild(mapText);
-	var_map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(mapTextA);
+	varMap.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(mapTextA);
  
 }
 google.maps.event.addDomListener(window, 'load', initialize);
