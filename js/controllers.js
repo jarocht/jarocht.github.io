b09692cd23c55b0c07f517d1686eb2f17cd32a08
Tim Jaroch <tim.jaroch@gmail.com>
2014-12-03 10:38:23 -0500

    Moved site to HTML5 navigation. Updated links accordingly. Portfolio page now uses hashtag for entry selection.

--- js/controllers.js
+++ js/controllers.js
@@ -1,35 +1,36 @@
 ﻿var application = angular.module('application', []);
 
-application.controller('portfolioCtrl', function ($scope, $http, $window) {
+application.config(function ($locationProvider) {
+    console.log("DONE THIS");
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
+
+application.controller('portfolioCtrl', function ($scope, $http, $window, $location) {
     //init vars
     $scope.index = 0;
-    $scope.Selected = '';
-    $scope.Records = '';
-
+    $scope.Selected = {};
+    $scope.Records = {};
 
     $http.get('./content/portfolio/data.json').success(function (data) {
         $scope.Records = data;
         $scope.Selected = $scope.Records["entries"][$scope.index];
+        if ($location.hash()) {
+            setRecord($location.hash());
+        }
     });
 
-    $scope.previous = function() {
-        if ($scope.index > 0) {
-            $scope.index = $scope.index - 1;
-            $scope.Selected = $scope.Records["entries"][$scope.index];
-        }
+
+    $scope.previous = function () {
+        setRecord($scope.index - 1, false);
     };
     $scope.next = function() {
-        if ($scope.index + 1 < $scope.Records["entries"].length) {
-            $scope.index = $scope.index + 1;
-            $scope.Selected = $scope.Records["entries"][$scope.index];
-        }
+        setRecord($scope.index + 1, false);
     };
     $scope.navTo = function(index) {
-        if (index > -1 && index < $scope.Records["entries"].length) {
-            $scope.index = index;
-            $scope.Selected = $scope.Records["entries"][$scope.index];
-            $window.scrollTo(0, 0);
-        }
+        setRecord(index, true);
     };
     $scope.getRecord = function(index) {
         if (index > -1 && index < $scope.Records["entries"].length) {
@@ -46,5 +47,16 @@ application.controller('portfolioCtrl', function ($scope, $http, $window) {
         if ($scope.Selected.image == null)
             return $scope.Records["defaultImage"];
         return $scope.Selected.image;
+    };
+
+    function setRecord(index, scroll) {
+        if (index > -1 && index < $scope.Records["entries"].length) {
+            $scope.index = index;
+            $location.hash(index);
+            $scope.Selected = $scope.Records["entries"][$scope.index];
+            if (scroll) {
+                $window.scrollTo(0, 0);
+            }
+        }
     };
 });
