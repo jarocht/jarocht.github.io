656f222edfc562c1ad6886d107d3dd50c432d73b
Tim Jaroch <tim.jaroch@inin.com>
2015-04-28 16:10:21 -0400

    Moved firebase db calls to factory and added async logic to reduce db calls across all controllers.

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,121 @@
 ﻿var app = angular.module('app');
 var baseUrl = "https://timjaroch.firebaseio.com/";
 
+app.factory('contentManagerFactory', function ($q, $firebaseObject, $firebaseArray) {
+    var service = {};
+
+    var portfolioPosts = undefined;
+    var portfolioContent = undefined;
+    var blogPosts = undefined;
+    var blogContent = undefined;
+
+    service.getPorfolioPosts = function () {
+        var def = $q.defer();
+        if (portfolioPosts == undefined) {
+            portfolioPosts = $firebaseArray(new window.Firebase(baseUrl + "portfolio/posts"));
+            portfolioPosts.$loaded(function() {
+                def.resolve(portfolioPosts);
+            });
+        } else {
+            def.resolve(portfolioPosts);
+        }
+
+        return def.promise;
+    }
+
+    service.getPorfolioPost = function(id) {
+        var def = $q.defer();
+        if (portfolioPosts == undefined) {
+            portfolioPosts = $firebaseArray(new window.Firebase(baseUrl + "portfolio/posts"));
+            portfolioPosts.$loaded(function () {
+                for (var i = 0; i < portfolioPosts.length; i++) {
+                    if (portfolioPosts[i].$id == id) {
+                        def.resolve(portfolioPosts[i]);
+                    }
+                }
+                def.reject();
+            });
+        } else {
+            for (var i = 0; i < portfolioPosts.length; i++) {
+                if (portfolioPosts[i].$id == id) {
+                    def.resolve(portfolioPosts[i]);
+                }
+            }
+            def.reject();
+        }
+
+        return def.promise;
+    }
+
+    service.getBlogPosts = function () {
+        var def = $q.defer();
+        if (blogPosts == undefined) {
+            blogPosts = $firebaseArray(new window.Firebase(baseUrl + "blog/posts"));
+            blogPosts.$loaded(function () {
+                def.resolve(blogPosts);
+            });
+        } else {
+            def.resolve(blogPosts);
+        }
+
+        return def.promise;
+    }
+
+    service.getBlogPost = function (id) {
+        var def = $q.defer();
+        if (blogPosts == undefined) {
+            blogPosts = $firebaseArray(new window.Firebase(baseUrl + "blog/posts"));
+            blogPosts.$loaded(function () {
+                for (var i = 0; i < blogPosts.length; i++) {
+                    if (blogPosts[i].$id == id) {
+                        def.resolve(blogPosts[i]);
+                    }
+                }
+                def.reject();
+            });
+        } else {
+            for (var i = 0; i < blogPosts.length; i++) {
+                if (blogPosts[i].$id == id) {
+                    def.resolve(blogPosts[i]);
+                }
+            }
+            def.reject();
+        }
+
+        return def.promise;
+    }
+
+    service.getPortfolioContent = function () {
+        var def = $q.defer();
+        if (portfolioContent == undefined) {
+            portfolioContent = $firebaseObject(new window.Firebase(baseUrl + "portfolio"));
+            portfolioContent.$loaded(function () {
+                def.resolve(portfolioContent);
+            });
+        } else {
+            def.resolve(portfolioContent);
+        }
+
+        return def.promise;
+    }
+
+    service.getBlogContent = function () {
+        var def = $q.defer();
+        if (blogContent == undefined) {
+            blogContent = $firebaseObject(new window.Firebase(baseUrl + "blog"));
+            blogContent.$loaded(function () {
+                def.resolve(blogContent);
+            });
+        } else {
+            def.resolve(blogContent);
+        }
+
+        return def.promise;
+    }
+
+    return service;
+});
+
 app.controller('aboutCtrl', function($scope) {
 });
 
@@ -16,53 +131,73 @@ app.controller('contactCtrl', function ($scope, $location) {
     initialize();
 });
 
-app.controller("contentPostCtrl", function ($scope, $firebaseObject, $stateParams, $state) {
-    console.log($stateParams);
-
-    var post = undefined;
-    var content = undefined;
-
+app.controller("contentPostCtrl", function ($scope, $firebaseObject, $stateParams, $state, contentManagerFactory) {
     if ($stateParams.type == "portfolio" && $stateParams.id != "") {
-        post = new window.Firebase(baseUrl + "portfolio/posts/" + $stateParams.id);
-        content = new window.Firebase(baseUrl + "portfolio");
+        contentManagerFactory.getPorfolioPost($stateParams.id).then(function (post) {
+            $scope.post = post;
+        }, function (error) {
+            $state.go("contentNavigator", {type: 'portfolio'});
+        });
+
+        contentManagerFactory.getPortfolioContent().then(function(content) {
+            $scope.title = content.title;
+            $scope.subTitle = content.subTitle;
+        });
     } else if ($stateParams.type == "blog" && $stateParams.id != "") {
-        post = new window.Firebase(baseUrl + "blog/posts/" + $stateParams.id);
-        content = new window.Firebase(baseUrl + "blog");
+        contentManagerFactory.getBlogPost($stateParams.id).then(function (post) {
+            $scope.post = post;
+        }, function (error) {
+            $state.go("contentNavigator", { type: 'blog' });
+        });
+
+        contentManagerFactory.getBlogContent().then(function (content) {
+            $scope.title = content.title;
+            $scope.subTitle = content.subTitle;
+        });
     } else {
         $state.go("about");
         return;
     }
-
-    content = $firebaseObject(content);
-    content.$loaded().then(function () {
-        $scope.title = content.title;
-        $scope.subTitle = content.subTitle;
-    });
-
-    post = $firebaseObject(post);
-    post.$loaded().then(function () {
-        $scope.post = post;
-    });
-
 });
 
-app.controller("contentNavigatorCtrl", function ($scope, $firebaseObject, $firebaseArray, $stateParams, $state) {
-
-    var posts = undefined;
-    var content = undefined;
+app.controller("contentNavigatorCtrl", function ($scope, $firebaseObject, $firebaseArray, $stateParams, $state, contentManagerFactory) {
     $scope.title = "Title Loading...";
     $scope.subTitle = "Subtitle Loading...";
     $scope.type = $stateParams.type;
 
     if ($stateParams.type == "portfolio") {
-        posts = new window.Firebase(baseUrl + "portfolio/posts");
-        content = new window.Firebase(baseUrl + "portfolio");
+        contentManagerFactory.getPorfolioPosts().then(function (data) {
+            $scope.posts = data;
+            provision();
+        });
+
+        contentManagerFactory.getPortfolioContent().then(function (content) {
+            $scope.title = content.title;
+            $scope.subTitle = content.subTitle;
+        });
+
+
     } else if ($stateParams.type == "blog") {
-        posts = new window.Firebase(baseUrl + "blog/posts");
-        content = new window.Firebase(baseUrl + "blog");
+        contentManagerFactory.getBlogPosts().then(function (data) {
+            $scope.posts = data;
+            provision();
+        });
+
+        contentManagerFactory.getBlogContent().then(function (content) {
+            $scope.title = content.title;
+            $scope.subTitle = content.subTitle;
+        });
     } else {
         $state.go("about");
         return;
+    }
+
+    function provision() {
+        $scope.postCount = $scope.posts.length;
+        $scope.selectedPage = 0;
+        $scope.postsPerPage = 3;
+        $scope.numPages = Math.ceil($scope.postCount / $scope.postsPerPage);
+        provisionPages();
     }
 
     function provisionPages() {
@@ -73,21 +208,6 @@ app.controller("contentNavigatorCtrl", function ($scope, $firebaseObject, $fireb
             $scope.pages[Math.floor(i / $scope.postsPerPage)].push($scope.posts.$getRecord($scope.posts.$keyAt(i)));
         }
     }
-
-    content = $firebaseObject(content);
-    content.$loaded().then(function () {
-        $scope.title = content.title;
-        $scope.subTitle = content.subTitle;
-    });
-
-    $scope.posts = $firebaseArray(posts);
-    $scope.posts.$loaded().then(function () {
-        $scope.postCount = $scope.posts.length;
-        $scope.selectedPage = 0;
-        $scope.postsPerPage = 3;
-        $scope.numPages = Math.ceil($scope.postCount / $scope.postsPerPage);
-        provisionPages();
-    });
 
     $scope.getPostList = function () {
         var postList = [];
